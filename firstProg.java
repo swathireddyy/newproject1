e6ce85a127400aa4c5374057813176f71a8d462f
swathireddyy <swathithillana12@gmail.com>
2020-06-29 13:42:01 +0530

    first commit changed in local system

--- firstProg.java
+++ firstProg.java
@@ -1,6 +1,6 @@
 public class firstProg{
 	public static void main(String[] args)
 {
-System.out.println("Hello world,Welcome to the fairy land tenalach");
+System.out.println("Hello world,Welcome to the fairy land tenalach,the far away land from humans");
 }
 }
