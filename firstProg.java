22f48ee118717090c6552ad96a55f76601a07d53
swathireddyy <swathithillana12@gmail.com>
2020-06-29 13:37:07 +0530

    first commit updated again

--- firstProg.java
+++ firstProg.java
@@ -1,6 +1,6 @@
 public class firstProg{
 	public static void main(String[] args)
 {
-System.out.println("Hello world,Welcome");
+System.out.println("Hello world,Welcome to the fairy land tenalach");
 }
 }
